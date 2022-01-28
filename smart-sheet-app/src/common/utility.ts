77deb2469a1671d8af8c2e5a5ab68df449a51cbc
uttesh <uttesh@gmail.com>
2022-01-28 22:52:37 +0530

    Added dynamic param field population

--- smart-sheet-app/src/common/utility.ts
+++ smart-sheet-app/src/common/utility.ts
@@ -1,4 +1,5 @@
 import { DeviceData } from "../services/devicedata.model";
+import { Device } from "../services/device.model";
 
 export const getDateTimeStr = (datestr: string) => {
   const _date = new Date(datestr);
@@ -9,8 +10,21 @@ export const getDateTimeStr = (datestr: string) => {
 
 export const getFormattedData = (data: string) => {
   const jdata = JSON.parse(data);
-  console.log("jdata :: ", jdata["data"]);
   return jdata["data"];
 };
 
-export const processData = (deviceDataList: DeviceData[]) => {};
+export const processData = (deviceDataList: DeviceData[], device: Device) => {
+  let processDataList = new Array();
+  deviceDataList.forEach((deviceData) => {
+    let processData = new Map();
+    device.params.split(",").forEach((param) => {
+      const jdata = JSON.parse(deviceData.data).data;
+      processData.set(param, JSON.parse(jdata)[param]);
+    });
+    processData.set("id", deviceData._id);
+    processData.set("createdDate", deviceData.createdDate);
+    const obj = Object.fromEntries(processData);
+    processDataList.push(obj);
+  });
+  return processDataList;
+};
--- smart-sheet-app/src/pages/devices/devices.details.tsx
+++ smart-sheet-app/src/pages/devices/devices.details.tsx
@@ -11,8 +11,15 @@ import {
   fetchDeviceDataById
 } from "../../services/devices.service";
 import { DateTime } from "luxon";
-import { getDateTimeStr, getFormattedData } from "../../common/utility";
+import {
+  getDateTimeStr,
+  getFormattedData,
+  processData
+} from "../../common/utility";
 import { Device } from "../../services/device.model";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 export interface DeviceDetailProps {}
 
@@ -20,35 +27,44 @@ export const DeviceDetail: FC<DeviceDetailProps> = () => {
   let { deviceId } = useParams();
   const [deviceData, setDeviceData] = useState<DeviceData[]>([]);
   const [device, setDevice] = useState<Device>(new Device());
+  const [processedDeviceData, setProcessedDeviceData] =
+    useState<Map<string, unknown>[]>();
 
-  const handleGetRowId = (e: any) => {
-    return e._id;
+  const columns: GridColDef[] = [];
+
+  const handleRowId = (e: any) => {
+    return e.id;
   };
 
-  const columns: GridColDef[] = [
-    {
-      field: "data",
-      headerName: "Data",
-      width: 600,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${getFormattedData(params.row.data)}`
-    },
-    {
+  const getColumns = () => {
+    columns.push({
       field: "createdDate",
       headerName: "Created Date",
       width: 250,
       valueGetter: (params: GridValueGetterParams) =>
         `${getDateTimeStr(params.row.createdDate)}`
-    }
-  ];
+    });
+
+    device.params.split(",").forEach((param) => {
+      columns.push({
+        field: param,
+        headerName: param,
+        width: 250
+      });
+    });
+    return columns;
+  };
 
   useEffect(() => {
     if (deviceId)
       fetchDeviceDataById(deviceId).then((data) => {
         setDeviceData(data);
+
         if (deviceId)
           fetchByDeviceId(deviceId).then((device) => {
             setDevice(device);
+            let _data = processData(data, device);
+            setProcessedDeviceData(_data);
           });
       });
   }, []);
@@ -64,16 +80,16 @@ export const DeviceDetail: FC<DeviceDetailProps> = () => {
           }
         }}
       >
-        {device && (
+        {processedDeviceData && processedDeviceData.length > 0 && (
           <Card>
             <CardHeader title={device.name} subheader={device.params} />
             <CardContent>
               <div style={{ height: 400, width: "100%" }}>
                 <DataGrid
-                  rows={deviceData}
-                  getRowId={handleGetRowId}
-                  columns={columns}
-                  pageSize={5}
+                  getRowId={handleRowId}
+                  rows={processedDeviceData}
+                  columns={getColumns()}
+                  pageSize={10}
                   rowsPerPageOptions={[5]}
                 />
               </div>
